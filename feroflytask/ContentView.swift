658b40ab41d6b78a6186c0f537a85a2afd5e79ab
Lalith <lalithpunepalli@gmail.com>
2020-04-04 18:51:18 +0530

    Initial Commit

---
+++ feroflytask/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  feroflytask
+//
+//  Created by Lalith on 04/04/20.
+//  Copyright © 2020 NANI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
