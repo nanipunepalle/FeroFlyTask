b1b24d8c27de551033e5f273b8c8b37647b0c8e7
Lalith <lalithpunepalli@gmail.com>
2020-04-05 15:02:13 +0530

    annotation added for mapview

--- feroflytask/MapView.swift
+++ feroflytask/MapView.swift
@@ -13,7 +13,6 @@ struct MapView: UIViewRepresentable{
     
     func makeUIView(context: Context) -> MKMapView {
            MKMapView(frame: .zero)
-           
        }
     
     func updateUIView(_ uiView: MKMapView, context: Context) {
@@ -21,13 +20,11 @@ struct MapView: UIViewRepresentable{
             latitude: 28.569130, longitude: 77.198059)
         let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
         let region = MKCoordinateRegion(center: coordinate, span: span)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
         uiView.setRegion(region, animated: true)
-//        UIVisualEffectView.addA
+        uiView.addAnnotation(annotation)
     }
-    
-   
-    
-    
 }
 
 struct MapView_Previews: PreviewProvider {
